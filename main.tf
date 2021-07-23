ef5b3d3b28d2a9ddd2994344f98f3cf66db2ba4c
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-23 20:47:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,17 @@ terraform {
 }
 
 locals {
-  vcn = data.oci_core_vcn.vcn
+  vcn_id = var.vcn_id
+  cidr = var.cidr
+  name = var.name
+  
+  vcn = data.oci_core_vcn.this
+  subnet = oci_core_subnet.this
+  routing_table = oci_core_route_table.this
 }
 
-data "oci_core_vcn" "vcn" {
-  vcn_id = var.vcn_id
+data "oci_core_vcn" "this" {
+  vcn_id = local.vcn_id
 }
 
 resource "oci_core_subnet" "this" {
@@ -23,30 +29,30 @@ resource "oci_core_subnet" "this" {
   vcn_id = local.vcn.id
   prohibit_public_ip_on_vnic = false
 
-  cidr_block = var.cidr
-  route_table_id = oci_core_route_table.this.id
-  display_name = var.name
-  dns_label = var.name
+  cidr_block = local.cidr
+  route_table_id = local.routing_table.id
+  display_name = local.name
+  dns_label = local.name
 }
 
 resource "oci_core_internet_gateway" "this" {
   compartment_id = local.vcn.compartment_id
   vcn_id = local.vcn.id
   
-  display_name = var.name
+  display_name = local.name
 }
 
 resource "oci_core_route_table" "this" {
   compartment_id = local.vcn.compartment_id
   vcn_id = local.vcn.id
 
-  display_name = var.name
+  display_name = local.name
   
-  route_rules {
+  route_rules = [{
     network_entity_id = oci_core_internet_gateway.this.id
     
-    description = "Internet route"
+    description = format("Internet route for %s subnet", local.name)
     destination_type = "CIDR_BLOCK"
     destination = "0.0.0.0/0"
-  }
+  }]
 }
