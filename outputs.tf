98f1485debf40945ec99a964dde3227cd1e17f62
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-23 20:50:24 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,4 @@
-output "subnet_id" {
-  value = oci_core_subnet.this.id
-}
-
 output "subnet" {
-  value = oci_core_subnet.this
+  value = local.subnet
+  description = "Provides the oci_core_subnet resource in Oracle Cloud Infrastructure Core service."
 }
