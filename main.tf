0dc3876e130ad4acf230b84aa030432b606c2784
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-24 16:29:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,26 +33,3 @@ module "subnet" {
   public = true
 }
 
-/*
-resource "oci_core_internet_gateway" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-  
-  display_name = local.name
-}
-
-resource "oci_core_route_table" "this" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-
-  display_name = local.name
-  
-  route_rules = [{
-    network_entity_id = oci_core_internet_gateway.this.id
-    
-    description = format("Internet route for %s subnet", local.name)
-    destination_type = "CIDR_BLOCK"
-    destination = "0.0.0.0/0"
-  }]
-}
-*/
