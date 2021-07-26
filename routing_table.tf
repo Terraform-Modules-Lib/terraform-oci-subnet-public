f9cbb7beab33f8851f2de6b02557ec56c3682bd1
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 09:13:08 +0200

    Update routing_table.tf

--- routing_table.tf
+++ routing_table.tf
@@ -5,7 +5,7 @@ resource "oci_core_route_table" "routing_table" {
   display_name = local.name
   
   route_rules {
-    network_entity_id = oci_core_internet_gateway.internet.id
+    network_entity_id = local.internet_gw_id
 
     description = format("Internet route for %s subnet", local.name)
     destination_type = "CIDR_BLOCK"
@@ -13,10 +13,3 @@ resource "oci_core_route_table" "routing_table" {
   }
 }
 
-
-resource "oci_core_internet_gateway" "internet" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-  
-  display_name = local.name
-}
